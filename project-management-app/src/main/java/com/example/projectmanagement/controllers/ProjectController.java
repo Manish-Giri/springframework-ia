617f534b5b59bc006225e0a6a8d7506513bc2f71
manish-giri <manish.giri.me@gmail.com>
2020-05-26 03:34:59 -0400

    Bind project object to new-project form

--- project-management-app/src/main/java/com/example/projectmanagement/controllers/ProjectController.java
+++ project-management-app/src/main/java/com/example/projectmanagement/controllers/ProjectController.java
@@ -1,7 +1,10 @@
 package com.example.projectmanagement.controllers;
 
+import com.example.projectmanagement.entities.Project;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -10,8 +13,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class ProjectController {
 
     @GetMapping("/new")
-    public String displayProjectForm() {
+    public String displayProjectForm(Model model) {
+        model.addAttribute("project", new Project());
         return "new-project";
         //return "index";
+    }
+
+    @PostMapping("/save")
+    public String createProject(Project project, Model model) {
+        return "result";
     }
 }
