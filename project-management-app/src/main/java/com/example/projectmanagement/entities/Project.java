2eb268828cbe079b3ddf9eec4aa4df516415b801
manish-giri <manish.giri.me@gmail.com>
2020-05-25 23:10:21 -0400

    Create Project class

---
+++ project-management-app/src/main/java/com/example/projectmanagement/entities/Project.java
@@ -0,0 +1,52 @@
+package com.example.projectmanagement.entities;
+
+public class Project {
+
+    private long projectId;
+
+    private String name;
+    private String stage;
+    private String description;
+
+    public Project() {
+        System.out.println("Default Project constructor...");
+    }
+
+    public Project(String name, String stage, String description) {
+        this.name = name;
+        this.stage = stage;
+        this.description = description;
+    }
+
+    public long getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(long projectId) {
+        this.projectId = projectId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStage() {
+        return stage;
+    }
+
+    public void setStage(String stage) {
+        this.stage = stage;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
