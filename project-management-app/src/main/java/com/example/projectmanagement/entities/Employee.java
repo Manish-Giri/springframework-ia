b78b0395629a2c631d8f9fd555cc96925482abcc
manish-giri <manish.giri.me@gmail.com>
2020-05-27 22:38:09 -0400

    Create Employee Entity

---
+++ project-management-app/src/main/java/com/example/projectmanagement/entities/Employee.java
@@ -0,0 +1,54 @@
+package com.example.projectmanagement.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long employeeId;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+
+
+    public Employee(long employeeId, String firstName, String lastName, String email) {
+        this.employeeId = employeeId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public Employee() {
+        System.out.println("Default constructor Employee.");
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
