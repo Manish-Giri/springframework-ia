3d4b3a86b03968d5cf140382522c3c5fc69c7588
manish-giri <manish.giri.me@gmail.com>
2020-05-28 20:49:24 -0400

    Create Employee Controller

---
+++ project-management-app/src/main/java/com/example/projectmanagement/controllers/EmployeeController.java
@@ -0,0 +1,19 @@
+package com.example.projectmanagement.controllers;
+
+import com.example.projectmanagement.entities.Employee;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    @GetMapping("/new")
+    public String displayEmployeeForm(Model model) {
+        model.addAttribute("employee", new Employee());
+        return "new-employee";
+        //return "index";
+    }
+}
