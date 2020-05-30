48dd180b0af368e305a8408388e7df7241f4aace
manish-giri <manish.giri.me@gmail.com>
2020-05-29 23:48:09 -0400

    Create HomeController to serve landing page

---
+++ project-management-app/src/main/java/com/example/projectmanagement/controllers/HomeController.java
@@ -0,0 +1,26 @@
+package com.example.projectmanagement.controllers;
+
+import com.example.projectmanagement.dao.ProjectRepository;
+import com.example.projectmanagement.entities.Project;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    // to get list of projects
+    @Autowired
+    ProjectRepository projectRepository;
+
+    // to serve app home page
+    @GetMapping("/")
+    public String displayHome(Model model) {
+        List<Project> projectList = projectRepository.findAll();
+        model.addAttribute("projectList", projectList);
+        return "main/home";
+    }
+}
