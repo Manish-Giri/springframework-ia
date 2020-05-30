72e595706a9d1f96827e309533cb9b56c46c6225
manish-giri <manish.giri.me@gmail.com>
2020-05-29 23:02:06 -0400

    Organize Thymeleaf views into directories

--- project-management-app/src/main/java/com/example/projectmanagement/controllers/ProjectController.java
+++ project-management-app/src/main/java/com/example/projectmanagement/controllers/ProjectController.java
@@ -8,7 +8,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/projects")
@@ -20,7 +19,7 @@ public class ProjectController {
     @GetMapping("/new")
     public String displayProjectForm(Model model) {
         model.addAttribute("project", new Project());
-        return "new-project";
+        return "projects/new-project";
         //return "index";
     }
 
